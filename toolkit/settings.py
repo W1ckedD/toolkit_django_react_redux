9584fe2a2e6ad6d5dd04e1d48ea71ab283e24535
w1ckedd <alirezachoobineh@ymail.com>
2022-08-02 13:17:57 +0430

    configured database

--- toolkit/settings.py
+++ toolkit/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'toolkit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'toolkit_django_react',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
     }
 }
 
